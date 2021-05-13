75273371d63c7c8ded771a1febef7f4c4b7deeab
pirateKing <dishanttanwar@gmail.com>
2021-05-13 14:42:26 +0530

    FIX: Circular Import on enum. - Changed enum.py name to validation_choices.py

--- projects/01_fyyur/starter_code/forms.py
+++ projects/01_fyyur/starter_code/forms.py
@@ -2,16 +2,16 @@ from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
 from wtforms.validators import DataRequired, AnyOf, URL
-from enum import Genre, State    
+from validation_choices import Genre, State    
 import re
 
 
 class ShowForm(Form):
     artist_id = StringField(
-        'artist_id'
+        'artist_id', validators=[DataRequired()]
     )
     venue_id = StringField(
-        'venue_id'
+        'venue_id', validators=[DataRequired()]
     )
     start_time = DateTimeField(
         'start_time',
@@ -34,10 +34,10 @@ class VenueForm(Form):
         'address', validators=[DataRequired()]
     )
     phone = StringField(
-        'phone'
+        'phone', validators=[DataRequired()]
     )
     image_link = StringField(
-        'image_link'
+        'image_link', validators=[DataRequired()]
     )
     genres = SelectMultipleField(
         # TODO implement enum restriction
@@ -83,7 +83,7 @@ class ArtistForm(Form):
      )
     facebook_link = StringField(
         # TODO implement enum restriction
-        'facebook_link' , validators=[URL()]
+        'facebook_link' , validators=[URL(), DataRequired()]
      )
 
     website_link = StringField(
